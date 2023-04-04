da826821e135c21a6916e9c02aa8cdddf3302561
bassem eloraby <BASEM12683@GMAIL.COM>
2023-04-04 19:27:45 +0300

    goal controller

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+// main routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log('Server listening on port'.yellow, port.blue);
+});
