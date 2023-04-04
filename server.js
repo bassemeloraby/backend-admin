df203d45d9b40a7bcbebf4667e0abf28767ed57d
bassem eloraby <BASEM12683@GMAIL.COM>
2023-04-04 21:24:30 +0300

    crude done deleteOne

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
@@ -13,7 +13,11 @@ app.use(express.json())
 // main routes
 app.use('/api/goals', require('./routes/goalRoutes'));
 
-app.listen(port, (err) => {
-  if (err) throw err;
-  console.log('Server listening on port'.yellow, port.blue);
-});
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(port, (err) => {
+      if (err) throw err;
+      console.log('Server listening on port'.red, port.yellow);
+    });      
+})
